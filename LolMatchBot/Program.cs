761157ec49f6290e00d692ea772fbf2adb3787d5
bubnick <braydon.bubnick@gmail.com>
2015-03-07 01:01:21 -0600

    Cleaned up code, added check to only check for comments in last day, added champ name to icon.

--- LolMatchBot/Program.cs
+++ LolMatchBot/Program.cs
@@ -17,176 +17,186 @@ namespace LolMatchBot
             var u = r.LogIn("LeagueMatch", "D6rCfKgzehv5wdLY");
             var s = r.GetSubreddit("/r/LeagueMatch");
             List<String> alreadyReplied = new List<String>();
-            foreach (var c in s.Comments.Take(50))
+
+            while(true)
             {
-                //This is going to be so inefficient eventually
-                //Maybe not since limited to 50 comments at once
-                //Eventually alreadyReplied will have to be moved to a master class, as we will be checking every several minutes
-                //And if it is not stored, it will be regenerated
-                //Check how reddit comment ids are generated, able to check based on last 50 somehow
-                if (c.Body.Contains("Match: ") && !alreadyReplied.Contains(c.Id))
+
+                DateTime yesterday = DateTime.Today.AddDays(-1);
+
+                foreach (var c in s.Comments.Take(50))
                 {
-
-                    try
+                    //This is going to be so inefficient eventually
+                    //Maybe not since limited to 50 comments at once
+                    //Eventually alreadyReplied will have to be moved to a master class, as we will be checking every several minutes
+                    //And if it is not stored, it will be regenerated
+                    //Check how reddit comment ids are generated, able to check based on last 50 somehow
+                    
+                    //Only check comments that are less than a day old
+                    if (c.Body.Contains("Match: ") && c.Created.CompareTo(yesterday) >= 0  && !alreadyReplied.Contains(c.Id))
                     {
-                        long matchID = long.Parse(c.Body.Substring(13, 11));
-                        var match = m.getMatch(matchID, "NA");
-                        var time = match.MatchDuration;
-                        String winner = "";
-                        long blueTotalKills = 0;
-                        long purpleTotalKills = 0;
 
-                        foreach (var team in match.Teams)
+                        try
                         {
-                            if (team.Winner)
-                            {
-                                winner = team.TeamId == 100 ? "Blue" : "Purple";
-                            }
-                        }
+                            long matchID = long.Parse(c.Body.Substring(13, 11));
+                            var match = m.getMatch(matchID, "NA");
+                            var time = match.MatchDuration;
+                            String winner = "";
+                            long blueTotalKills = 0;
+                            long purpleTotalKills = 0;
 
-                        foreach (var p in match.Participants)
-                        {
-                            if (p.TeamId == 100)
+                            foreach (var team in match.Teams)
                             {
-                                blueTotalKills += p.Stats.Kills;
+                                if (team.Winner)
+                                {
+                                    winner = team.TeamId == 100 ? "Blue" : "Purple";
+                                }
                             }
-                            else
-                            {
-                                purpleTotalKills += p.Stats.Kills;
-                            }
-                        }
 
-                        StringBuilder sb = new StringBuilder();
-                        sb.AppendLine("**Match ID: " + matchID + "**\n\n" + winner + " won " + (winner == "Blue" ? (blueTotalKills + "-" + purpleTotalKills) : (purpleTotalKills + "-" + blueTotalKills)) + " in " + time.ToString() + "\n");
-                        sb.AppendLine("***Blue***\n");
-                        sb.AppendLine("Champion | Level | KDA | Gold |  CS | Build");
-                        sb.AppendLine("---------|----------|----------|----------|----------|----------");
-                        foreach (var p in match.Participants)
-                        {
-                            //If blue
-                            if (p.TeamId == 100)
+                            foreach (var p in match.Participants)
                             {
-                                long totalCS = p.Stats.MinionsKilled + p.Stats.NeutralMinionsKilled;
-                                List<string> items = new List<string>();
+                                if (p.TeamId == 100)
+                                {
+                                    blueTotalKills += p.Stats.Kills;
+                                }
+                                else
+                                {
+                                    purpleTotalKills += p.Stats.Kills;
+                                }
+                            }
+
+                            StringBuilder sb = new StringBuilder();
+                            sb.AppendLine("**Match ID: " + matchID + "**\n\n***" + winner + " team won " + (winner == "Blue" ? (blueTotalKills + "-" + purpleTotalKills) : (purpleTotalKills + "-" + blueTotalKills)) + " in " + time.ToString() + "\n");
+                            sb.AppendLine("***Blue***\n");
+                            sb.AppendLine("Champion | Level | KDA | Gold |  CS | Build");
+                            sb.AppendLine("---------|----------|----------|----------|----------|----------");
+                            foreach (var p in match.Participants)
+                            {
+                                //If blue
+                                if (p.TeamId == 100)
+                                {
+                                    long totalCS = p.Stats.MinionsKilled + p.Stats.NeutralMinionsKilled;
+                                    List<string> items = new List<string>();
                                 
-                                if(p.Stats.Item0 != null && p.Stats.Item0 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item0));
-                                }
+                                    if(p.Stats.Item0 != null && p.Stats.Item0 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item0));
+                                    }
 
-                                if(p.Stats.Item1 != null && p.Stats.Item1 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item1));
-                                }
+                                    if(p.Stats.Item1 != null && p.Stats.Item1 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item1));
+                                    }
 
-                                if(p.Stats.Item2 != null && p.Stats.Item2 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item2));
-                                }
+                                    if(p.Stats.Item2 != null && p.Stats.Item2 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item2));
+                                    }
 
-                                if(p.Stats.Item3 != null && p.Stats.Item3 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item3));
-                                }
+                                    if(p.Stats.Item3 != null && p.Stats.Item3 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item3));
+                                    }
 
-                                if(p.Stats.Item4 != null && p.Stats.Item4 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item4));
-                                }
+                                    if(p.Stats.Item4 != null && p.Stats.Item4 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item4));
+                                    }
 
-                                if(p.Stats.Item5 != null && p.Stats.Item5 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item5));
-                                }
+                                    if(p.Stats.Item5 != null && p.Stats.Item5 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item5));
+                                    }
 
-                                if(p.Stats.Item6 != null && p.Stats.Item6 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item6));
-                                }
+                                    if(p.Stats.Item6 != null && p.Stats.Item6 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item6));
+                                    }
 
-                                sb.Append(m.getChampIcon(p.ChampionId) + " | " + p.Stats.ChampLevel + " | " + p.Stats.Kills + "/" + p.Stats.Deaths + "/" + p.Stats.Assists + " | " + p.Stats.GoldEarned + " | " + totalCS + " | ");
-                                string strItems = "";
-                                foreach(string str in items)
-                                {
-                                    strItems += str + ", ";
+                                    sb.Append(m.getChampIcon(p.ChampionId) + " | " + p.Stats.ChampLevel + " | " + p.Stats.Kills + "/" + p.Stats.Deaths + "/" + p.Stats.Assists + " | " + p.Stats.GoldEarned + " | " + totalCS + " | ");
+                                    string strItems = "";
+                                    foreach(string str in items)
+                                    {
+                                        strItems += str + ", ";
                                     
+                                    }
+                                    strItems = strItems.Substring(0, strItems.Length - 2);
+                                    sb.AppendLine(strItems);
                                 }
-                                strItems = strItems.Substring(0, strItems.Length - 2);
-                                sb.AppendLine(strItems);
                             }
-                        }
-                        sb.AppendLine("\n***Purple***\n");
-                        sb.AppendLine("Champion | Level | KDA | Gold |  CS | Build");
-                        sb.AppendLine("---------|----------|----------|----------|----------|----------");
-                        foreach (var p in match.Participants)
-                        {
-                            //If blue
-                            if (p.TeamId == 200)
+                            sb.AppendLine("\n***Purple***\n");
+                            sb.AppendLine("Champion | Level | KDA | Gold |  CS | Build");
+                            sb.AppendLine("---------|----------|----------|----------|----------|----------");
+                            foreach (var p in match.Participants)
                             {
-                                long totalCS = p.Stats.MinionsKilled + p.Stats.NeutralMinionsKilled;
-                                List<string> items = new List<string>();
-
-                                if (p.Stats.Item0 != null && p.Stats.Item0 != 0)
+                                //If blue
+                                if (p.TeamId == 200)
                                 {
-                                    items.Add(m.getItem(p.Stats.Item0));
-                                }
+                                    long totalCS = p.Stats.MinionsKilled + p.Stats.NeutralMinionsKilled;
+                                    List<string> items = new List<string>();
 
-                                if (p.Stats.Item1 != null && p.Stats.Item1 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item1));
-                                }
+                                    if (p.Stats.Item0 != null && p.Stats.Item0 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item0));
+                                    }
 
-                                if (p.Stats.Item2 != null && p.Stats.Item2 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item2));
-                                }
+                                    if (p.Stats.Item1 != null && p.Stats.Item1 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item1));
+                                    }
 
-                                if (p.Stats.Item3 != null && p.Stats.Item3 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item3));
-                                }
+                                    if (p.Stats.Item2 != null && p.Stats.Item2 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item2));
+                                    }
 
-                                if (p.Stats.Item4 != null && p.Stats.Item4 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item4));
-                                }
+                                    if (p.Stats.Item3 != null && p.Stats.Item3 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item3));
+                                    }
 
-                                if (p.Stats.Item5 != null && p.Stats.Item5 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item5));
-                                }
+                                    if (p.Stats.Item4 != null && p.Stats.Item4 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item4));
+                                    }
 
-                                if (p.Stats.Item6 != null && p.Stats.Item6 != 0)
-                                {
-                                    items.Add(m.getItem(p.Stats.Item6));
-                                }
+                                    if (p.Stats.Item5 != null && p.Stats.Item5 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item5));
+                                    }
 
-                                sb.Append(m.getChampIcon(p.ChampionId) + " | " + p.Stats.ChampLevel + " | " + p.Stats.Kills + "/" + p.Stats.Deaths + "/" + p.Stats.Assists + " | " + p.Stats.GoldEarned + " | " + totalCS + " | ");
-                                string strItems = "";
-                                foreach (string str in items)
-                                {
-                                    strItems += str + ", ";
+                                    if (p.Stats.Item6 != null && p.Stats.Item6 != 0)
+                                    {
+                                        items.Add(m.getItem(p.Stats.Item6));
+                                    }
 
+                                    sb.Append(m.getChampIcon(p.ChampionId) + " | " + p.Stats.ChampLevel + " | " + p.Stats.Kills + "/" + p.Stats.Deaths + "/" + p.Stats.Assists + " | " + p.Stats.GoldEarned + " | " + totalCS + " | ");
+                                    string strItems = "";
+                                    foreach (string str in items)
+                                    {
+                                        strItems += str + ", ";
+
+                                    }
+                                    strItems = strItems.Substring(0, strItems.Length - 2);
+                                    sb.AppendLine(strItems);
                                 }
-                                strItems = strItems.Substring(0, strItems.Length - 2);
-                                sb.AppendLine(strItems);
                             }
+
+                            sb.AppendLine("\n^^I ^^am ^^a ^^bot! ^^summon ^^me ^^with ^^'Match: ^^1234567890'. ^^Currently ^^NA ^^only!");
+                            sb.AppendLine("\n^^Maintained ^^by ^^/u/bubnick ^^v0.1");
+                            c.Reply(sb.ToString());
+                            alreadyReplied.Add(c.Id);
+                            Console.WriteLine("Commented on commentID: " + c.Id + ", matchID: " + matchID);
+                        }
+                        catch (Exception exp)
+                        {
+
                         }
 
-                        sb.AppendLine("\n^^I ^^am ^^a ^^bot! ^^summon ^^me ^^with ^^'Match: ^^1234567890'. ^^Currently ^^NA ^^only!");
-                        sb.AppendLine("\n^^Maintained ^^by ^^/u/bubnick ^^v0.1");
-                        c.Reply(sb.ToString());
-                        alreadyReplied.Add(c.Id);
                     }
-                    catch (Exception exp)
-                    {
-
-                    }
-
                 }
+
+
             }
-
-
         }
     }
 }
